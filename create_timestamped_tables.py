c88eb2e1a4a9a51425989bb8de7867ca73598ed7
Craig Silverstein <csilvers@khanacademy.org>
2016-09-21 12:08:55 -0700

    Set up logging to log to stdout unless there's an error.
    Summary:
    The reason we care is this is run from cron, which will send stdout to
    a logfile and stderr out as mail.
    
    I also log timestamps, which will help a lot with debugging.
    
    Send WARNING/ERROR to both stdout *and* stderr.
    
    That way it shows up in the logfile as well as being send in email.
    
    I also fixed a bug where I was ignoring the `verbose` flag by accident.
    
    Test Plan:
    I ran
       ./create_timestamped_tables.py -n > /dev/null
    which produced no output, whereas before this change it will still
    show the log messages.
    
    (same)
    
    Reviewers: samantha, colin
    
    Reviewed By: colin
    
    Differential Revision: https://phabricator.khanacademy.org/D30930

--- create_timestamped_tables.py
+++ create_timestamped_tables.py
@@ -379,18 +379,13 @@ def setup_logging(verbose):
     The reason we care is this is run from cron, which will send stdout
     to a logfile and stderr out as mail.
 
-    This is taken from
+    This will cause us to log WARNING/ERROR twice when run from the
+    commandline (once to stdout and once to stderr), but it seems
+    worth it to get the errors in the logfile as well as in email.
+
+    This is based of an idea at
     http://stackoverflow.com/questions/2302315/how-can-info-and-debug-logging-message-be-sent-to-stdout-and-higher-level-messag
     """
-    class LessThanFilter(logging.Filter):
-        def __init__(self, exclusive_maximum, name=""):
-            super(LessThanFilter, self).__init__(name)
-            self.max_level = exclusive_maximum
-
-        def filter(self, record):
-            #non-zero return means we log this message
-            return 1 if record.levelno < self.max_level else 0
-
     logger = logging.getLogger()
     # Have to set the root logger level, it defaults to logging.WARNING.
     logger.setLevel(logging.NOTSET)
@@ -399,8 +394,7 @@ def setup_logging(verbose):
     formatter = logging.Formatter(logs_format)
 
     logging_handler_out = logging.StreamHandler(sys.stdout)
-    logging_handler_out.setLevel(logging.DEBUG)
-    logging_handler_out.addFilter(LessThanFilter(logging.WARNING))
+    logging_handler_out.setLevel(logging.DEBUG if verbose else logging.INFO)
     logging_handler_out.setFormatter(formatter)
     logger.addHandler(logging_handler_out)
 
